cabb3fcd25f60a255d37b3780783156f13a996a7
Julian Toledano <jtoledanodiaz@gmail.com>
2020-03-02 15:50:14 +0100

    Import sql added.
     * UPDATE: the db is now created with some persons in it.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO person(id, name, surname, age)
+VALUES (nextval('hibernate_sequence'), 'Sarah', 'Connor', 45);
+INSERT INTO person(id, name, surname, age)
+VALUES (nextval('hibernate_sequence'), 'John', 'Connor', 25);
+INSERT INTO person(id, name, surname, age)
+VALUES (nextval('hibernate_sequence'), 'Terminator', 'Swat', 105);
